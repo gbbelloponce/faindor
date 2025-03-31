f5fb68e09e77672a0aa25512759daf708cf2f42d
gbbelloponce <gbbelloponce@gmail.com>
2025-03-30 22:36:08 -0300

    add theme toggler in auth routs

--- apps/web-ui/src/app/(auth)/login/page.tsx
+++ apps/web-ui/src/app/(auth)/login/page.tsx
@@ -1,27 +1,36 @@
 import Link from "next/link";
 
 import { Logo } from "@/components/logo";
+import { ThemeToggler } from "@/components/theme-toggler";
 import { LoginForm } from "./login-form";
 
 export default function Login() {
 	return (
-		<div className="min-h-screen flex flex-col items-center justify-center">
-			<div className="w-full max-w-md space-y-8">
-				<div className="flex flex-col items-center space-y-2 text-center">
-					<Logo size="md" withTitle />
-					<h1 className="text-4xl font-bold">Welcome back</h1>
-					<p className="text-muted-foreground">
-						Log in to connect with your organization
-					</p>
-				</div>
-				<LoginForm />
-				<div className="text-center text-sm">
-					Don't have an account?{" "}
-					<Link href="/register" className="font-medium text-primary underline">
-						Register here
-					</Link>
+		<>
+			<header className="absolute top-2 right-2">
+				<ThemeToggler buttonVariant="ghost" />
+			</header>
+			<div className="min-h-screen flex flex-col items-center justify-center">
+				<div className="w-full max-w-md space-y-8">
+					<div className="flex flex-col items-center space-y-2 text-center">
+						<Logo size="md" withTitle />
+						<h1 className="text-4xl font-bold">Welcome back</h1>
+						<p className="text-muted-foreground">
+							Log in to connect with your organization
+						</p>
+					</div>
+					<LoginForm />
+					<div className="text-center text-sm">
+						Don't have an account?{" "}
+						<Link
+							href="/register"
+							className="font-medium text-primary underline"
+						>
+							Register here
+						</Link>
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
--- apps/web-ui/src/app/(auth)/register/page.tsx
+++ apps/web-ui/src/app/(auth)/register/page.tsx
@@ -1,27 +1,33 @@
 import Link from "next/link";
 
 import { Logo } from "@/components/logo";
+import { ThemeToggler } from "@/components/theme-toggler";
 import { RegisterForm } from "./register-form";
 
 export default function Register() {
 	return (
-		<div className="min-h-screen flex flex-col items-center justify-center">
-			<div className="w-full max-w-md space-y-8">
-				<div className="flex flex-col items-center space-y-2 text-center">
-					<Logo size="md" withTitle />
-					<h1 className="text-4xl font-bold">Create an account</h1>
-					<p className="text-muted-foreground">
-						Join your organization's network
-					</p>
-				</div>
-				<RegisterForm />
-				<div className="text-center text-sm">
-					Already have an account?{" "}
-					<Link href="/login" className="font-medium text-primary underline">
-						Log In
-					</Link>
+		<>
+			<header className="absolute top-2 right-2">
+				<ThemeToggler buttonVariant="ghost" />
+			</header>
+			<div className="min-h-screen flex flex-col items-center justify-center">
+				<div className="w-full max-w-md space-y-8">
+					<div className="flex flex-col items-center space-y-2 text-center">
+						<Logo size="md" withTitle />
+						<h1 className="text-4xl font-bold">Create an account</h1>
+						<p className="text-muted-foreground">
+							Join your organization's network
+						</p>
+					</div>
+					<RegisterForm />
+					<div className="text-center text-sm">
+						Already have an account?{" "}
+						<Link href="/login" className="font-medium text-primary underline">
+							Log In
+						</Link>
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
