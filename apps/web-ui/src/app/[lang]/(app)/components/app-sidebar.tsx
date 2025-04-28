5aa312ec70fcd6a39a8b208662e1665eeb3206a9
gbbelloponce <gbbelloponce@gmail.com>
2025-04-28 20:06:50 -0300

    feat(web-ui): add sidebar items

--- apps/web-ui/src/app/[lang]/(app)/components/app-sidebar.tsx
+++ apps/web-ui/src/app/[lang]/(app)/components/app-sidebar.tsx
@@ -7,10 +7,35 @@ import {
 	SidebarFooter,
 } from "@/components/ui/sidebar";
 
+import { Calendar, Home, ImageIcon, ShoppingBag, Users } from "lucide-react";
+
+const navItems = [
+	{ icon: Home, href: "/home", label: "Home" },
+	{ icon: Calendar, href: "/calendar", label: "Events" },
+	{ icon: Users, href: "/communities", label: "Communities" },
+	{ icon: ImageIcon, href: "/photos", label: "Photos" },
+	{ icon: ShoppingBag, href: "/marketplace", label: "Marketplace" },
+];
+
 export function AppSidebar() {
 	return (
 		<Sidebar className="top-16 !h-[calc(100vh-4rem)]">
-			<SidebarContent className="p-6">Sidebar</SidebarContent>
+			<SidebarContent className="p-6 flex flex-col gap-2">
+				<nav>
+					<ul className="flex list-none flex-col gap-2">
+						{navItems.map((navItem) => (
+							<li key={navItem.href} className="flex w-full">
+								<Link
+									href={navItem.href}
+									className="flex items-center hover:bg-accent rounded-full p-3 w-full h-full"
+								>
+									{<navItem.icon className="mr-2 h-5 w-5" />} {navItem.label}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</nav>
+			</SidebarContent>
 			<SidebarFooter className="p-6 flex items-center">
 				<Link
 					href="https://github.com/gbbelloponce/faindor"
